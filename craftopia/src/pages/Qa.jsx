055f378eadcd64c65f94342dcf8af892a5c318d8
MingYuan200 <yuan40133@gmail.com>
2024-12-15 19:56:50 +0800

    1215

--- craftopia/src/pages/Qa.jsx
+++ craftopia/src/pages/Qa.jsx
@@ -3,10 +3,24 @@ import './qa.scss'
 import Blob from "../assets/blob.svg";
 import Blob2 from "../assets/blob2.svg";
 import Blob3 from "../assets/blob3.svg";
+import { useRef, useState } from 'react';
 
 
 
 export default function Qa() {
+    // 一開始就要先渲染sctive1 所以要先在useState給值
+    const [activeId, setActiveId] = useState('qaPart1');
+    const refPart1 = useRef();
+    const refPart2 = useRef();
+    const refPart3 = useRef();
+
+    const handleClick = (id, ref) => {
+        setActiveId(id);
+        if (ref.current) {
+            ref.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <>
             {/* <!-- 主要內容區 --> */}
@@ -15,59 +29,80 @@ export default function Qa() {
             </section>
 
 
-
             <main>
                 <div className="content-bg">
                     {/* 麵包穴 */}
                     <div className="bread2">
-                        <a href="">首頁</a>
+                        <a href="#">首頁</a>
                         <img src="./images/icon-tag/type=arrowRight_bold.svg" alt="" />
-                        <a href="">常見問題</a>
+                        <a href="/qa">常見問題</a>
                     </div>
 
                     <div className="content">
                         <ul className="member-QA-left">
-                            <li>
-                                <a className="left-list" href="">
-                                    <Blob className='leftIcon'></Blob>
-                                    會員相關
-                                </a>
-                            </li>
-                            <li>
-                                <a className="left-list" href="">
-                                    <Blob2 className='leftIcon'></Blob2>
-                                    課程相關
-                                </a>
-                            </li>
-                            <li>
-                                <a className="left-list" href="">
-                                    <Blob3 className='leftIcon'></Blob3>
-                                    預約相關
-                                </a>
-                            </li>
+                            <div className="QaLeftSticky">
+                                <li>
+                                    <a className="left-list" href="#" 
+                                    onClick={(e) => {
+                                            e.preventDefault(); // 阻止預設行為
+                                            handleClick('qaPart1', refPart1);
+                                        }}>
+                                        <Blob className={`leftIcon  ${activeId === 'qaPart1' ? 'active' : ''}`}></Blob>
+                                        會員相關
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="left-list" href="#" 
+                                    onClick={(e) => {
+                                        e.preventDefault(); // 阻止預設行為
+                                        handleClick('qaPart2', refPart2);
+                                    }}>
+                                        <Blob2 className={`leftIcon ${activeId === 'qaPart2' ? 'active' : ''}`}></Blob2>
+                                        課程相關
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="left-list" href="#" 
+                                    onClick={(e) => {
+                                        e.preventDefault(); // 阻止預設行為
+                                        handleClick('qaPart3', refPart3);
+                                    }}>
+                                        <Blob3 className={`leftIcon ${activeId === 'qaPart3' ? 'active' : ''}`}></Blob3>
+                                        預約相關
+                                    </a>
+                                </li>
+                            </div>
+
                         </ul>
 
                         <div className="member-QA-right">
-                            <h2 className="title">
-                                <img src="./images/title/會員相關.svg" alt="" />
-                            </h2>
-                            <ul className="list">
-                                <ShowQa />
-                            </ul>
+                            <section id='qaPart1' ref={refPart1}>
+                                <h2 className="title">
+                                    <img src="./images/title/會員相關.svg" alt="" />
+                                </h2>
+                                <ul className="list">
+                                    <ShowQa />
+                                </ul>
+                            </section>
 
-                            <h2 className="title">
-                                <img src="./images/title/會員相關.svg" alt="" />
-                            </h2>
-                            <ul className="list">
-                                <ShowQa />
-                            </ul>
-                            
-                            <h2 className="title">
-                                <img src="./images/title/會員相關.svg" alt="" />
-                            </h2>
-                            <ul className="list">
-                                <ShowQa />
-                            </ul>
+                            <section id='qaPart2' ref={refPart2}>
+                                <h2 className="title">
+                                    <img src="./images/title/課程相關.svg" alt="" />
+                                </h2>
+                                <ul className="list">
+                                    <ShowQa />
+                                </ul>
+                            </section>
+
+                            <section id='qaPart3' ref={refPart3}>
+                                <h2 className="title">
+                                    <img src="./images/title/預約相關.svg" alt="" />
+                                </h2>
+                                <ul className="list">
+                                    <ShowQa />
+                                </ul>
+                            </section>
+
                         </div>
                     </div>
                 </div>
